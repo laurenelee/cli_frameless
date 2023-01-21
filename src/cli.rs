2a03b9dd72a9fef2f650b0db8ddba674ae516c98
Lauren Lee <lauren@lolocoding.com>
2023-01-21 09:59:51 -0300

    adding dependencies to make the extrinsic calls work

--- src/cli.rs
+++ src/cli.rs
@@ -1,28 +1,81 @@
 use std::io;
 use std::io::stdin;
 use console::{ Term, Style };
+use reqwest;
+use serde_json;
+use serde::Deserialize;
+use std::thread;
+use std::time::Duration;
+use parity_scale_codec::{Decode, Encode};
+use sp_core::{hexdisplay::HexDisplay, OpaqueMetadata, H256};
 
 
+
+#[derive(Deserialize)]
+pub struct RPCResponses {
+    // jsonrpc: String,
+    result: String,
+    // id: u8,
+}
+#[derive(Encode, Decode, Debug, PartialEq, Eq, Clone)]
+pub enum Call {
+	// from, to, amount, fee
+	Transfer([u8; 32], [u8; 32], u32, u32),
+	Upgrade(Vec<u8>),
+	NewAccount([u8; 32]),
+	Mint([u8; 32]),
+}
+#[derive(Debug, Encode, Decode, PartialEq, Eq, Clone)]
+pub struct BasicExtrinsic(Call);
+
+impl BasicExtrinsic {
+	fn new_unsigned(call: Call) -> Self {
+		<Self as sp_runtime::traits::Extrinsic>::new(call, None).unwrap()
+	}
+}
+
+impl sp_runtime::traits::Extrinsic for BasicExtrinsic {
+	type Call = Call;
+	type SignaturePayload = ();
+
+	fn new(data: Self::Call, _: Option<Self::SignaturePayload>) -> Option<Self> {
+		Some(Self(data))
+	}
+}
 
 pub fn run(term: &Term) -> io::Result<()> {
     let cyan = Style::new().cyan();
     let ferris = Style::new().color256(214).bold();
 
-    term.write_line(&ferris.apply_to("--- Welcome! ---").to_string())?;
-
+    term.write_line(&ferris.apply_to("--- Welcome! Let's send some LOLO loot ---").to_string())?;
+    thread::sleep(Duration::from_millis(900));
     // find out if you have to create an account 
     let account_question = &cyan.apply_to("Do you have an LOLO account? (y/n)").to_string();
     let account_response: String = get_string(&account_question, &term);
 
     if account_response == String::from("n") {
-        // create an account for them
+        let user_number = get_u32_input("Okay, give me a number then and I'll create an account for you:", &term) as u8;
+        let new_account = BasicExtrinsic::new_unsigned(Call::NewAccount([user_number; 32]));
+
+        println!("new account_1: {:?}", HexDisplay::from(&new_account.encode()));
+        // user needs a new account created 
         // call new account extrinsic and give [u8; 32]
-        // 
+        let client = reqwest::blocking::Client::new();
+        let res = client.post("http:/localhost:9933/")
+            .json(&serde_json::json!({
+                "jsonrpc": "2.0",
+                "id": 1,
+                "method": "author_submitExtrinsic",
+                "params": ["020c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c"]
+             }))
+            .send();
+            let response_body: RPCResponses = res.unwrap().json().unwrap();
+        println!("{:?}", response_body.result);
+        
         // call mint extrinsic 
         // welcome! by joining, you've minted 10 lolo tokens 
         // rpc call out & create a new account 
         // but for now we'll set it manually 
-        let id: u32 = 4;
     } else {
         let id: u32 = get_u32_input("What is your account id?", &term);
     }
@@ -32,7 +85,8 @@ pub fn run(term: &Term) -> io::Result<()> {
     if action_response == String::from("S") || action_response == String::from("s") {
         // send tokens 
         let recipient_id: u32 = get_u32_input("Who do you want to send to? (their account id)", &term);
-        let amount: u32 = get_u32_input("How many LOLO tokens do you want to send?", &term);
+        let amount: u32 = get_u32_input("How many LOLO loot do you want to send?", &term);
+        let fee: u32 = get_u32_input("What is the fee you want to pay? (Must be > 1)", &term);
         // RPC CALL to send tokens 
             // logic for whether it was successful or not
         // show ending balance 
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,37 @@
-// use console::Term;
-use reqwest;
-use serde_json;
-use serde::Deserialize;
+use console::Term;
+// use reqwest;
+// use serde_json;
+// use serde::Deserialize;
+
 // use serde_json::Result;
 
 
-// mod cli;
-#[derive(Deserialize)]
-pub struct RPCResponses {
-    jsonrpc: String,
-    result: String,
-    id: u8,
-}
+mod cli;
+// #[derive(Deserialize)]
+// pub struct RPCResponses {
+//     // jsonrpc: String,
+//     result: String,
+//     // id: u8,
+// }
 
 fn main() {
+    // let client = reqwest::blocking::Client::new();
+    // let res = client.post("http:/localhost:9933/")
+    //     .json(&serde_json::json!({
+    //         "jsonrpc": "2.0",
+    //         "id": 1,
+    //         "method": "author_submitExtrinsic",
+    //         "params": ["000c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c02020202020202020202020202020202020202020202020202020202020202020300000001000000"]
+    //      }))
+    //     .send();
+    //     let response_body: RPCResponses = res.unwrap().json().unwrap();
+    // println!("{:?}", response_body.result);
 
 
-    let client = reqwest::blocking::Client::new();
-    let res = client.post("http:/localhost:9933/")
-        .json(&serde_json::json!({
-            "jsonrpc": "2.0",
-            "id": 1,
-            "method": "author_submitExtrinsic",
-            "params": ["020c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c"]
-         }))
-        .send();
-        let response_body: RPCResponses = res.unwrap().json().unwrap();
+    let term = Term::stdout();
 
-    println!("{:?}", response_body.result);
-
-
-    // let term = Term::stdout();
-
-    // match cli::run(&term) {
-    //     Err(e) => println!("{:?}", e),
-    //     _ => (),
-    // }
+    match cli::run(&term) {
+        Err(e) => println!("{:?}", e),
+        _ => (),
+    }
 }
